1ff80c6bf2b52db98b8528dad9a20e4cbd3118f9
Adam Ransom <v0id.gmr@gmail.com>
2015-10-18 23:13:30 +0200

    initial commit with basic functions
    1. Runs with just a status icon
    2. Can open a YouTube embed URL into a new window
    3. Able to load multiple windows
    4. Remebers where windows are positioned for next time

---
+++ Overwatch/AppDelegate.swift
@@ -0,0 +1,120 @@
+//
+//  AppDelegate.swift
+//  Overwatch
+//
+//  Created by adam on 10/18/15.
+//  Copyright (c) 2015 Adam Ransom. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var statusMenu: NSMenu!
+
+  var videoWindows = [NSWindow]()
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    let icon = NSImage(named: "StatusIcon")
+    icon?.template = true
+
+    statusItem.image = icon
+    statusItem.menu = statusMenu
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+  @IBAction func actionOpenURL(sender: NSMenuItem) {
+    let urlField = NSTextField(frame: CGRectMake(0, 0, 300, 20))
+    let alert = NSAlert()
+
+    alert.messageText = "Enter the URL of the YouTube video you want to watch:"
+    alert.addButtonWithTitle("OK")
+    alert.addButtonWithTitle("Cancel")
+    alert.accessoryView = urlField
+
+    let result = alert.runModal()
+    switch(result) {
+    case NSAlertFirstButtonReturn:
+      createWindow(urlField.stringValue)
+    default:
+      break
+    }
+  }
+
+  @IBAction func actionResetPositions(sender: NSMenuItem) {
+    for (index, window) in videoWindows.enumerate() {
+      window.setFrame(frameForWindow(index), display: false, animate: true)
+    }
+  }
+
+  @IBAction func actionQuit(sender: NSMenuItem) {
+    NSApplication.sharedApplication().terminate(self)
+  }
+
+  /*
+   * Create a new video window
+   */
+  func createWindow(url: String) {
+    let windowIndex = videoWindows.count
+
+    // Default position and size for window
+    let frame = frameForWindow(windowIndex)
+
+    // Create a new NSWindow with default properties
+    let videoWindow = NSWindow(
+      contentRect: frame,
+      styleMask: NSTitledWindowMask | NSFullSizeContentViewWindowMask | NSResizableWindowMask | NSClosableWindowMask,
+      backing: .Buffered,
+      `defer`: false
+    )
+
+    // Style the window
+    videoWindow.titlebarAppearsTransparent = true
+    videoWindow.movableByWindowBackground = true
+    videoWindow.standardWindowButton(.ZoomButton)?.hidden = true
+    videoWindow.standardWindowButton(.MiniaturizeButton)?.hidden = true
+
+    // Create a NSWindowController to manage the window
+    let videoWindowController = NSWindowController(window: videoWindow)
+    videoWindowController.windowFrameAutosaveName = "overwatch_video_window_\(windowIndex)"
+
+    // If available, restore the last size and position of the window
+    videoWindow.setFrameUsingName("overwatch_video_window_\(windowIndex)")
+
+    // Create a new VideoViewController to display the video
+    let videoViewController = VideoViewController(nibName: nil, bundle: nil)!
+    videoViewController.url = NSURL(string: url)
+    videoWindow.contentView!.addSubview(videoViewController.view)
+    videoViewController.view.frame = (videoWindow.contentView! as NSView).bounds
+
+    // Add the window the the global list of opened windows
+    videoWindows.append(videoWindow)
+
+    // Finally, show the window!
+    videoWindow.makeKeyWindow()
+    videoWindowController.showWindow(nil)
+  }
+
+  /*
+   * Get the default position for a particlar window index
+   */
+  func frameForWindow(index: Int) -> CGRect {
+    // Default to a 16:9 ratio for YouTube
+    let width: CGFloat = 400
+    let height: CGFloat = 225
+
+    // Default to top-right, with multiple windows stacking below
+    let padding: CGFloat = 20
+    let x = (NSScreen.mainScreen()?.visibleFrame.width)! - width - padding
+    let y = (NSScreen.mainScreen()?.visibleFrame.height)! - (height + padding) * CGFloat(index + 1)
+
+    return CGRectMake(x, y, width, height)
+  }
+
+}
+
---
+++ Overwatch/VideoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  VideoViewController.swift
+//  Overwatch
+//
+//  Created by adam on 10/18/15.
+//  Copyright (c) 2015 Adam Ransom. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class VideoViewController: NSViewController {
+
+  var url: NSURL?
+  private var webView: WKWebView?
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+  override init?(nibName: String!, bundle: NSBundle!) {
+    super.init(nibName: nibName, bundle: bundle)
+  }
+
+  override func loadView() {
+    webView = WKWebView()
+    view = webView!
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if (url != nil) {
+      let req = NSURLRequest(URL: url!)
+      webView?.loadRequest(req)
+    }
+  }
+}
